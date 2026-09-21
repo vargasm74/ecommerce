8807d23ec997b51fc05a66815ff7e8c63ed5b5f8
vargasm74 <66799944+vargasm74@users.noreply.github.com>
2026-09-21 15:50:09 -0300

    fix: limpiar condicional duplicado de Facebook

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -214,7 +214,6 @@ JAVASCRIPT PERSONALIZADO
 <script src="<?php echo $url; ?>vistas/js/carrito-de-compras.js"></script>
 
 <?php if(!empty($facebookOauthEnabled)){ ?>
-<?php if(!empty($facebookOauthEnabled)){ ?>
 <!--=====================================
 https://developers.facebook.com/
 ======================================-->
@@ -240,7 +239,6 @@ https://developers.facebook.com/
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 </script>
-<?php } ?>
 <?php } ?>
 
 </body>
