f54763404bbbfd9e5dbb07864526e66c63d1e3f0
vargasm74 <66799944+vargasm74@users.noreply.github.com>
2026-09-21 12:30:00 -0300

    security: endurecer configuracion de sesiones

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -1,3 +1,27 @@
+<?php
+
+if(session_status() !== PHP_SESSION_ACTIVE){
+
+    $secureCookie = filter_var(
+        getenv("SESSION_SECURE_COOKIE") ?: "false",
+        FILTER_VALIDATE_BOOLEAN
+    );
+
+    ini_set("session.use_strict_mode", "1");
+    ini_set("session.use_only_cookies", "1");
+
+    session_set_cookie_params([
+        "lifetime" => 0,
+        "path" => "/",
+        "secure" => $secureCookie,
+        "httponly" => true,
+        "samesite" => "Lax"
+    ]);
+
+    session_start();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -16,7 +40,6 @@
 
 	<?php
 
-		session_start();
 
 		$servidor = Ruta::ctrRutaServidor();
 
